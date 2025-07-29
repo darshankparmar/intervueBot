43592128e4198890c2fbf269f6f4ceed6efcfb60
hiren9979 <hirenchavda141@gmail.com>
2025-07-29 22:39:44 +0530

    fix

--- backend/routes/document_routes.py
+++
@@ -1,9 +0,0 @@
-# routes/document_routes.py
-from fastapi import APIRouter, UploadFile, File
-from controllers.document_controller import handle_document_upload
-
-router = APIRouter()
-
-@router.post("/upload")
-async def upload_document(file: UploadFile = File(...)):    
-    return await handle_document_upload(file)
